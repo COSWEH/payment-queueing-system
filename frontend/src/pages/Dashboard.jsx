9160e17690cc5d5e7b4ba4882f5108c73cfec551
ooap <yesimrealmarv@gmail.com>
2025-02-12 16:25:09 +0800

    add comment for customer Dashboard

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -4,6 +4,7 @@ import QueueList from "../components/QueueList";
 const Dashboard = () => {
   return (
     <div className="mt-4 mb-4 px-4 flex flex-col justify-center items-center min-h-screen">
+      {/* for customer */}
       <QueueList />
     </div>
   );
