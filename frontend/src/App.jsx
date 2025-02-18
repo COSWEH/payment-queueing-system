fd27edd3579338898ae98d037a824bd61de98d8d
ooap <yesimrealmarv@gmail.com>
2025-02-18 09:44:57 +0800

    add icon to frontend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
-
+import { Frown } from "lucide-react";
 // pages
 import CustomerNumber from "./pages/CustomerNumber";
 import Dashboard from "./pages/Dashboard";
@@ -23,16 +23,20 @@ const App = () => {
           <Route path="/login" element={<Login />} />
           <Route path="/teller" element={<TellerDashboard />} />
 
-          {/* 404 */}
+          {/* 404 Page Not Found */}
           <Route
             path="*"
             element={
-              <div className="flex items-center justify-center bg-gray-200 h-screen">
-                <div className="text-center text-white bg-orange-500 p-6 rounded-md shadow-lg">
-                  <Link to="/" className="font-extrabold text-xl">
-                    Go back
-                  </Link>
-                </div>
+              <div className="flex flex-col items-center justify-center bg-gray-100 h-screen">
+                <Frown className="text-green-500" size={125} />
+                <h1 className="text-6xl font-bold text-green-500 mb-2">404</h1>
+                <p className="text-gray-700 text-xl mb-6">Page not found</p>
+                <Link
+                  to="/"
+                  className="bg-green-500 text-white px-6 py-3 rounded-md shadow-lg hover:bg-green-600 transition-colors"
+                >
+                  Back to Home
+                </Link>
               </div>
             }
           />
